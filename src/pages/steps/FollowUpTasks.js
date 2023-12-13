5ddae0a633ea365e0d36e6951a0865ab61075b8b
Marmik Pandya <marmikpandya@gmail.com>
2023-12-13 22:12:17 +0530

    enable texting

--- src/pages/steps/FollowUpTasks.js
+++ src/pages/steps/FollowUpTasks.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio } from '@mui/material';
+import { TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Typography, Box } from '@mui/material';
 
 function FollowUpTasks({ formData, onFormDataChange }) {
     const handleChange = (event) => {
@@ -9,16 +9,11 @@ function FollowUpTasks({ formData, onFormDataChange }) {
     // Tasks and a grid where we can add new tasks. Each task as task type - Extraction, Summarize, SMS, What'sapp, calendar invite something else  
     // Enable automated rescheduling of the call  
     return (
-        <form>
-            <TextField
-                label="Name of Agent"
-                variant="outlined"
-                name="agentName"
-                value={formData.agentName || ''}
-                onChange={handleChange}
-            />
-            {/* Add other fields for step 1 */}
-        </form>
+        <Box>
+            <Typography variant='h4'> Follow up tasks </Typography>
+            <Typography variant='caption'> Coming soon </Typography>
+
+        </Box>
     );
 }
 
