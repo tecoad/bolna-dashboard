5f39f6991dbadd638d2d0141faacba2f6e2d920c
Prateek Sachan <ps@prateeksachan.com>
2024-01-14 23:16:03 +0530

    API changes

--- src/pages/assistant-details/RunsTable.js
+++ src/pages/assistant-details/RunsTable.js
@@ -1,27 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
 import { Box } from '@mui/material';
 import JsonTable from '../../components/Table';
 import fakeData from '../../data/fake_run_details.json';
+import createApiInstance from '../../utils/api';
 
-function RunTable({ }) {
+
+function RunTable({ accessToken }) {
     const location = useLocation();
     const [runData, setRunData] = useState([]);
     const [loading, setLoading] = useState(false);
     const agent = location.state?.agent;
     const userId = location.state?.userId;
     const agentId = agent?.range.split("#")[1];
+    const api = createApiInstance(accessToken);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 setLoading(true);
-                const response = await axios.get(`${process.env.REACT_APP_FAST_API_BACKEND_URL}/agent/executions?user_id=${userId}&assistant_id=${agentId}`);
-                var runs = []
-                if (response.data && response.data.runs && response.data.runs.length > 0) {
-                    runs = [...response.data.runs];
-                }
+                const response = await api.get(`/agent/executions?agent_id=${agentId}`);
+                var runs = [...response.data.data]
                 setRunData(runs);
                 setLoading(false);
                 console.log(`Got all executions and this is run data ${JSON.stringify(runData)}`)
@@ -32,7 +31,7 @@ function RunTable({ }) {
 
         };
         fetchData();
-    }, [agentId, userId]); // Empty dependency array ensures this effect runs once on component mount
+    }, [agentId, accessToken]); // Empty dependency array ensures this effect runs once on component mount
 
 
     return (
