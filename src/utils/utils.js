045f820096685992d6dc37260d75e90469ef54c1
Marmik Pandya <marmikpandya@gmail.com>
2024-01-18 08:50:35 +0530

    make default type as wav

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -28,7 +28,7 @@ export const CREATE_AGENT_FORM = {
     },
     engagementConfig: {
         channel: 'Websocket',
-        format: 'mp3'
+        format: 'wav'
     },
     rulesConfig: {
         prompts: {
