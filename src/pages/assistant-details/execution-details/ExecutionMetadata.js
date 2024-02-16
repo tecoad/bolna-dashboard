6842b8b475e6cca32851ca4bc4c67db8a5937d1c
Prateek Sachan <ps@prateeksachan.com>
2024-02-16 16:46:29 +0530

    fix

--- src/pages/assistant-details/execution-details/ExecutionMetadata.js
+++ src/pages/assistant-details/execution-details/ExecutionMetadata.js
@@ -42,7 +42,7 @@ function ExecutionMetadata({ executionDetails }) {
                     "Run Duration": `${executionDetails.conversation_time} seconds`,
                     "Run Date": new Date(executionDetails.createdAt).toLocaleDateString(),
                     "Total Credits Used": `${executionDetails.total_cost.toFixed(5)}`,
-                    "Telephony Used": executionDetails.telephony_data?.hosted_twilio ? "bolna hosted" : "self"
+                    "Telephony Used": executionDetails.telephony_data?.hosted_telephony ? "bolna hosted" : "self hosted"
                 })}
                 {renderSection("Usage Breakdown", {
                     "Transcriber Model": executionDetails?.usage_breakdown?.transcriberModel,
